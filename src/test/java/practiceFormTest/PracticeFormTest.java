8aaad4e85d3d2af30a0532e378b9d7580cbd7c55
Ilya T <IlyaT123@yandex.ru>
2024-03-18 22:22:29 +0300

    @Tag("UI") Убрано

--- src/test/java/practiceFormTest/PracticeFormTest.java
+++ src/test/java/practiceFormTest/PracticeFormTest.java
@@ -5,13 +5,12 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import step.FormSteps;
 
-@Tag("UI")
+
 public class PracticeFormTest extends BaseTest {
     FormSteps formSteps = new FormSteps();
     @Test
     @DisplayName("Проверка регистрации формы demoqa.com")
     public void testChekingRegistrationFormDemoQA() {
-
         formSteps.openFormRegistration()
                 .setFullFormaRegistr()
                 .verificationSuccessfulRegistrationForm();
